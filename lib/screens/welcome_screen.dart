83803525f1be658d48eaa1b101ab2ff6b2bf747a
Burak Saler <burakksaler@gmail.com>
2021-04-19 19:51:46 +0300

    draw blueprint of welcome_screen

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -9,6 +9,23 @@ class WelcomeScreen extends StatefulWidget {
 class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              child: Text('login'),
+            ),
+            Container(
+              child: Text('register'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
